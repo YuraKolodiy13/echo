fdadb9627c2ca82c509f04758e64cf341fd77c61
YuraKolodiy13 <Timberwolves13>
2019-03-15 17:47:49 +0200

    removing not using files

--- wp-content/themes/echo/footer.php
+++ wp-content/themes/echo/footer.php
@@ -1,14 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: yukolodiy
- * Date: 15.03.19
- * Time: 14:10
- */
-
-?>
-32323
-
 
 </body>
 </html>
--- wp-content/themes/echo/templates/homepage.php
+++ wp-content/themes/echo/templates/homepage.php
@@ -9,6 +9,7 @@
 
 <div class="banner">
     <h1><?php the_field('banner_title'); ?></h1>
+    <p><?php the_field('banner_text'); ?></p>
 </div>
 
 <?php get_footer(); ?>
